46cddc6895dfe923b532a29ddc478e33512990b6
Jay Konkin <jaykonkin@Jays-MacBook-Air.local>
2019-09-19 11:24:03 -0700

    validationService returns a status

--- src/Controller/UrlInfoController.php
+++ src/Controller/UrlInfoController.php
@@ -5,19 +5,20 @@ namespace App\Controller;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\HttpFoundation\Request;
+    use App\Service\UrlValidationService;
 
     class UrlInfoController extends AbstractController
     {
-        public function index(Request $request)
+        public function index(Request $request, UrlValidationService $urlValidationService)
 
         {
 
-            $routeName = $request->attributes->get('_route');
+            //$routeName = $request->attributes->get('_route');
             $routeParameters = $request->attributes->get('_route_params');
-
-            var_dump($routeParameters);
+            $isValid = $urlValidationService->validateURl($routeParameters);
+            //var_dump($routeParameters);
             return new Response(
-                '<html><body>'.$routeName.'</body><html>'
+                '<html><body>'.$isValid.'</body><html>'
             );
 
         }
--- src/Service/UrlValidationService.php
+++ src/Service/UrlValidationService.php
@@ -1,7 +1,12 @@
 <?php
 
+namespace App\Service;
 
 class UrlValidationService
 {
+    public function validateURl(array $urltovalidate)
+    {
 
+        return "blocked";
+    }
 }
